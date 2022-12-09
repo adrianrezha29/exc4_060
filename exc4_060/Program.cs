99fdb93f128af37a8b38c71d05c4d121fe378329
adrianrezha29 <adrianrezha29@gmail.com>
2022-12-09 16:32:19 +0700

    add case 2

--- exc4_060/Program.cs
+++ exc4_060/Program.cs
@@ -79,9 +79,16 @@ namespace exc4_060
                         int num = Convert.ToInt32(Console.ReadLine());
                         s.push(num);
                         break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
                 }
             }
-            
         }
     }
 }
